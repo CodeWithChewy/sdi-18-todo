78557a7ff966eeec92b115c9455bcb147bf7487e
codewithchewy <srepp@twu.edu>
2023-07-18 08:34:58 -0700

    Added js functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// hello 
+console.log('My code is running');
+let toDoItems = [];
+const addToDoItem = (item) => {
+    const toDoList = document.getElementById('todo-input');
+};
+
+const btn = document.querySelector('todo-form'); 
+btn.addEventListener("click", myClickFunction);
+
+const myClickFunction = () => {
+    
+} 
+
+ {
+    const input = document.getElementById('todo-input');
+    addToDoItem(input.value)
+    input.value = "";
+})
