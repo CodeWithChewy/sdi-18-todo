ddb388efb6a81f2e95d471488bfd14fa589d8e30
codewithchewy <srepp@twu.edu>
2023-07-18 09:35:08 -0700

    Trouble shoot

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ console.log('My code is running');
 let toDoItems = [];
 const addToDoItem = (item) => {
     const toDoList = document.getElementById('todo-input');
+
+    const li = document.createElement('li');
+    li.innerText = item;
 };
 
-const btn = document.querySelector('todo-form'); 
-btn.addEventListener("click", myClickFunction);
+let btn = document.getElementById("todo-form"); 
+
 
 const myClickFunction = () => {
-    
-} 
-
- {
     const input = document.getElementById('todo-input');
     addToDoItem(input.value)
     input.value = "";
-})
+    
+};
+console.log(btn)
+btn.addEventListener("click", myClickFunction);
