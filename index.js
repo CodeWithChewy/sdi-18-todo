b446eef919adb5a7915437c12ffe4bbd67d53cf5
codewithchewy <srepp@twu.edu>
2023-07-18 11:58:25 -0700

    Need style

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 console.log('My code is running');
 // let toDoItems = [];
 const addToDoItem = (item) => {
-    const toDoList = document.getElementById('todo-input');
+    const toDoList = document.getElementById('todo-list');
 
     const li = document.createElement('li');
     li.innerText = item;
+
+        li.addEventListener('click', () => {
+            toDoList.removeChild(li);
+        })
+
+      toDoList.appendChild(li);
 };
 
-let btn = document.getElementById("todo-form"); 
+let form = document.getElementById("todo-form"); 
 
-const myClickFunction = () => {
-    const input = document.getElementById('todo-input');
+const mySubmitFunction = (e) => {
+  e.preventDefault();  
+  const input = document.getElementById('todo-input');
     addToDoItem(input.value)
     input.value = "";
 };
-console.log(btn)
-btn.addEventListener("click", myClickFunction);
+console.log(form)
+form.addEventListener("submit", mySubmitFunction);
